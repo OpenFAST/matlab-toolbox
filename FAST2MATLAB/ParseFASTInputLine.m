b8eeb4f94dcb7503c5197bcb13ce81ef546ef9a2
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2013-03-10 04:51:16 +0000

    reordered some I/O in ElastoDyn and ServoDyn.f90 Compiles/have not run (needs some work first)
    git-svn-id: https://windsvn2.nrel.gov/FAST/branches/BJonkman/Source/UlilityCodes/SimulationToolbox@239 10acb478-4768-415a-8850-bacdb5912d4d

--- FAST2MATLAB/ParseFASTInputLine.m
+++ FAST2MATLAB/ParseFASTInputLine.m
@@ -1,4 +1,30 @@
-function [value, label, isComment] = ParseFASTInputLine( line )
+function [value, label, isComment, descr, fieldType] = ParseFASTInputLine( line )
+% This routine parses a line from a FAST type input file. 
+% Comment lines are assumed to start with any of the following characters:
+%   #!-=
+% If the line is not a comment, it is assumed to be of the form:
+%  value <old values> label descr
+%--------------------------------------------------------------------------
+% Inputs:
+%   line        - a line of text
+% Outputs:
+%   value       - the value of the parameter
+%   label       - the name of the parameter/variable/field
+%   isComment   - logical value that says if the line is a comment line
+%   descr       - the description of the line
+%   fieldType   - text saying the field is either a
+%                   "Comment", "Logical", "Character", "Numeric" variable
+%--------------------------------------------------------------------------
+
+        % first check that this isn't a blank line...
+    if isempty(line) || length(strtrim(line)) < 1
+        value = '';
+        label = '';
+        isComment = true;
+        descr = '';
+        fieldType = 'Comment';
+        return
+    end
 
     trueFalseValues = {'true','false','t','f'};
 
@@ -6,23 +32,29 @@ function [value, label, isComment] = ParseFASTInputLine( line )
     firstChar = sscanf(strtrim(line),'%c',1); %read the first non-whitespace character
     
     if ~isempty( strfind( '#!-=', firstChar ) ) %comments start with any of these characters: # ! - =
-        isComment = true;
         value = strtrim(line);
         label = '';
+        isComment = true;
+        descr = value;
+        fieldType = 'Comment';
     else
         isComment = false;
 
         % Get the Value, number or string
         [value, cnt, ~, nextindex] = sscanf(line,'%f',1);  %First check if line begins with a number
 
-        if cnt == 0
-            [testVal, position] = textscan(line,'%q',1);  %If not look for a string instead
-            if any( strcmpi(testVal,trueFalseValues) )
-                value = testVal;                %this is a logical input
+        if cnt == 0 % we didn't find a number so...
+            [testVal, position] = textscan(line,'%q',1);  %look for a string instead
+            if any( strcmpi(testVal{1}{1},trueFalseValues) )
+                value = testVal{1}{1};                %this is a logical input
+                fieldType = 'Logical';            
             else
                 value = ['"' testVal{1}{1} '"']; %add quotes back around the string
+                fieldType = 'Character';            
             end
             nextindex = position + 1;
+        else
+            fieldType = 'Numeric';            
         end
 
         % Now get the label     
@@ -34,7 +66,8 @@ function [value, label, isComment] = ParseFASTInputLine( line )
 
         IsLabel = false;
         label   = '';  %initialize in case the line doesn't have a label
-
+        descr   = '';
+        
         while ~IsLabel && length(line) >= nextindex
             line = line(nextindex:end);
 
@@ -47,6 +80,7 @@ function [value, label, isComment] = ParseFASTInputLine( line )
                 else
                     IsLabel = true;
                     label = testVal;
+                    descr = strtrim(line(nextindex:end));
                 end
             end
 
