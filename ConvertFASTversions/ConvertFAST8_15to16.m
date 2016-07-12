813b204a2b61f57d117b2ad51958561e092e3634
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2016-07-12 16:59:13 +0000

    * added matlab script to convert FAST 8.15 input files to 8.16 * removed section where I zero-out the number of inputs/outputs for linearization because I'm pretty sure it will cause a seg fault down the line (I haven't implemented the rest of the logic for LinInputs and LinOutputs, yet)
    git-svn-id: https://windsvn2.nrel.gov/FAST/branches/BJonkman/Utilities/SimulationToolbox@1293 10acb478-4768-415a-8850-bacdb5912d4d

---
+++ ConvertFASTversions/ConvertFAST8_15to16.m
@@ -0,0 +1,89 @@
+function ConvertFAST8_15to16(oldFSTName, newDir)
+%function ConvertFAST8_15to16(oldFSTName, newDir)
+% by Bonnie Jonkman
+%
+%Conversion of FAST v 8.15.x files to FAST v8.16.x
+%  based on "Demonstration of fast file manipuation" by Paul Fleming
+% (c) 2016 National Renewable Energy Laboratory
+%--------------------------------------------------------------------------
+% Required inputs:
+%  oldFSTName - the name of the old (v8) primary FAST input file,
+%               including full path name
+%  newDir     - the new directory that will contain converted input files
+%               (FAST 8.16.0);
+%               No other input files will be copied or moved.
+%
+% File requirements/assumptions for oldFSTName:
+% 1) Comment lines are assumed to start with any of the following four
+%      indicators (not including the quotes here):  "#", "!", "=", "--"
+%    (Header lines do not need to meet this requirement.)
+% 2) If the line is not a comment, it is assumed to be of the form:
+%      value [,Array values] <old values> label descr
+%    (String values cannot contain old values between the value and label.)
+% 3) There MUST be space between quoted strings and the variable name
+%
+% NOTE that Fortran allows input arrays to be separated by either spaces
+% or commas, but this toolbox currently requires them to be commas
+%.........................................................................
+%bjj: + perhaps we need to put an indication of whether we can allow old
+%       values or if array values are indicated by spaces instead of just
+%       commas
+
+%% let's get the directory that contains the template files
+
+thisFile    = which('ConvertFAST8_15to16');
+thisDir     = fileparts(thisFile);
+templateDir = strcat(thisDir,filesep, 'TemplateFiles' );
+
+        % Primary input file:
+
+[oldDir, baseName, ext ] = fileparts(oldFSTName);
+baseFileName  = strcat(baseName,ext);                 %base FAST file name
+newFSTname    = [newDir filesep baseFileName];
+
+if strcmpi(oldDir,newDir)
+    disp('ConvertFAST8_12to15 Warning:New FAST input file is overwriting old file.')
+end
+
+
+    %----------------------------------------------------------------------
+    % Load in old model data from the primary FAST and AeroDyn15 input files:
+    %----------------------------------------------------------------------
+    % primary file:
+
+    fprintf( '%s\n', '****************************************************');
+    fprintf( '%s\n', ['Converting ' baseFileName ':'] );
+    fprintf( '%s\n', [' old name: ' oldFSTName ] );
+    fprintf( '%s\n', [' new name: ' newFSTname ] );
+    fprintf( '%s\n', '****************************************************');
+
+
+        %Primary FAST file
+    inputfile = [oldDir filesep baseFileName];
+    FP = Fast2Matlab(inputfile,2); %FP are Fast Parameters, specify 2 lines of header (FAST 8)
+
+%%  %----------------------------------------------------------------------
+    % Get AD Data and write new AD15 file:
+    %----------------------------------------------------------------------
+    CompAero = GetFastPar(FP,'CompAero');
+    if CompAero == 2
+        FullADFile = GetFastPar(FP,'AeroFile');
+        [newADName]  = GetFullFileName( FullADFile, newDir ); % new path + name
+        [FullADFile] = GetFullFileName( FullADFile, oldDir );
+        ADPar = Fast2Matlab(FullADFile,2); % get AeroDyn data (2 header lines)
+
+        template   = [templateDir filesep 'AD_Primary_v15.03.x.dat'];  %template for primary file
+        Matlab2FAST(ADPar,template,newADName, 2); %contains 2 header lines
+    end    
+    
+    
+
+%%  %----------------------------------------------------------------------
+    % Write new model data to the FAST input files:
+    %----------------------------------------------------------------------
+    template   = [templateDir filesep 'FAST_Primary_v8.16.x.dat'];  %template for primary file
+    Matlab2FAST(FP,template,newFSTname, 2); %contains 2 header lines
+
+return
+
+end
