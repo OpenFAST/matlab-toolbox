a0a1d3b800a8352f714a0f924cb3011ee49c0619
Bonnie Jonkman <bonnie.jonkman@envision-energy.com>
2018-01-22 11:54:42 -0700

    Updated for BD linearization
    - BD descriptions include the blade number as "BD_#"
    - needed to reorder the states in the final matrices so that they are all modules' displacements followed by all modules' velocities instead of being ordered by module first

--- Source/findBladeTriplets.m
+++ Source/findBladeTriplets.m
@@ -1,11 +1,11 @@
 function [Triplets, NTriplets] = findBladeTriplets(rotFrame,Desc )
 
 %% Find the number of, and indices for, triplets in the rotating frame:
-    chkStr = {'[Bb]lade \d', '[Bb]lade [Rr]oot \d', '[Bb]\d', '\d'};
+    chkStr = {'[Bb]lade \d', '[Bb]lade [Rr]oot \d', 'BD_\d', '[Bb]\d', '\d'};
 
     NTriplets = 0;              % first initialize to zero
     Triplets = [];
-    for i = 1:length(rotFrame)    % loop through inputs/outputs
+    for i = 1:length(rotFrame)  % loop through inputs/outputs
         if rotFrame(i)          % this is in the rotating frame
             Tmp = zeros(1,3);
             foundIt = false;
@@ -14,26 +14,32 @@ function [Triplets, NTriplets] = findBladeTriplets(rotFrame,Desc )
                 if ~isempty(BldNoCol)
                     foundIt = true;
 
+                        % create another regular expression to find the
+                        % exact match on a different blade:
                     str = regexp(Desc{i},BldNoCol{1},'split'); %this should return the strings before and after the match
                     FirstStr = [str{1} BldNoCol{1}(1:end-1) '.' ];
                     checkThisStr = [FirstStr str{2}];
-                    checkThisStr = strrep(strrep(strrep(checkThisStr,')','\)'), '(', '\('),'^','\^'); %we need to get rid of the special characters
+                    
+                        %we need to get rid of the special characters that
+                        %may exist in Desc{}:
+                    checkThisStr = strrep(strrep(strrep(checkThisStr,')','\)'), '(', '\('),'^','\^'); 
+                    
                     k = str2double(BldNoCol{1}(end));
                     Tmp(k) = i;
                     break;
-                end % check the next one;
+                end % check the next regular expression if necessary;
             end
 
                 % find the other match values
             if foundIt 
-                for j = (i+1):length(rotFrame)	% loop through all remaining control inputs
-                    if rotFrame(j)          % this is in the rotating frame
+                for j = (i+1):length(rotFrame)           % loop through all remaining control inputs
+                    if rotFrame(j)                       % this is in the rotating frame
                         BldNoCol = regexp(Desc{j},checkThisStr,'match'); % match all but the blade number
                         if ~isempty(BldNoCol)                      
                             Num = regexp(Desc{j},FirstStr,'match'); % match all but the blade number
                             k = str2double(Num{1}(end));
-                            Tmp(k) = j; % save the indices for the remaining blades                   
-                            if ( all(Tmp ~=0) )                     % true if all the elements of Tmp are nonzero; thus, we found a triplet of rotating indices
+                            Tmp(k) = j;                             % save the indices for the remaining blades
+                            if ( all(Tmp) )                         % true if all the elements of Tmp are nonzero; thus, we found a triplet of rotating indices
                                 NTriplets = NTriplets + 1;          % this  is  the number  of  control input triplets in the rotating frame
                                 Triplets(NTriplets,:) = Tmp;        % these are the indices for control input triplets in the rotating frame
                                 break;
