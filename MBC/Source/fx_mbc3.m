09eb035128a3d10e608b75ab0a9d5741b932f670
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2020-01-15 15:55:09 -0700

    Small fixes for compatibility with Octave

--- MBC/Source/fx_mbc3.m
+++ MBC/Source/fx_mbc3.m
@@ -371,6 +371,7 @@ function WriteDataForVTK(VTK, ModeVizFileName)
         fwrite(fid, VTK.x_eig_magnitude(:,iMode,:), fileFmt);
         fwrite(fid, VTK.x_eig_phase(    :,iMode,:), fileFmt);
     end
+    fclose(fid)
    
 return;
 end
