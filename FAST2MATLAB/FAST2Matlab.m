OpenFAST/matlab-toolbox

commit 4a847ae9384e7b7ba2d233599c1471f9722ccb59

Fast2Matlab: Adds capability of properly reading MoorDyn input files - Adds parsing of MoorDyn line types, connection properties and line properties table as a formatted table. - Lets FAST2Matlab know that the output parameter section of the MoorDyn input file is often labeled 'OUTPUTS' (not 'OutList') and adjusts FAST2Matlab to be able to properly processes the list of outputs in MoorDyn input file correctly.

FAST2MATLAB/FAST2Matlab.m
@@ -102,21 +102,21 @@ while true %loop until discovering Outlist or end of file, than break
         break
     end
     
-        % Check to see if the value is Outlist
+        % Check to see if the value is Outlist or OUTPUTS (for MoorDyn)
 
     %if ~isempty(strfind(upper(line),upper('OutList'))) 
-    if ~isempty(strfind( upper(line), upper('OutList') )) 
+    if ~isempty(strfind( upper(line), upper('OutList') )) || contains(upper(line),upper('OUTPUTS'))
         % 6/23/2016: linearization inputs contain "OutList" in the
         % comments, so we need to make sure this is either the first (value) or
         % second (label) word of the line.
         [value, ~, ~, nextindex] = sscanf(line,'%s', 1); 
-        if strcmpi(value,'OutList')
+        if strcmpi(value,'OutList') || strcmpi(value,'OUTPUTS')
             [DataOut.OutList DataOut.OutListComments] = ParseFASTOutList(fid);
             break; %bjj: we could continue now if we wanted to assume OutList wasn't the end of the file...
         else
             % try the second
             [value] = sscanf(line(nextindex+1:end),'%s', 1); 
-            if strcmpi(value,'OutList')
+            if strcmpi(value,'OutList') || strcmpi(value,'OUTPUTS')
                 [DataOut.OutList DataOut.OutListComments] = ParseFASTOutList(fid);
                 break; %bjj: we could continue now if we wanted to assume OutList wasn't the end of the file...
             end
@@ -202,6 +202,20 @@ while true %loop until discovering Outlist or end of file, than break
             line = fgetl(fid);  % the next line is the header, and it may have comments
             [DataOut.profile] = ParseFASTNumTable(line, fid, NumUSRz, 2 );
             continue; %let's continue reading the file
+            
+        elseif strcmpi(value,'"Name"') %we've reached the MoorDyn line types table (and we think it's a string value so it's in quotes)
+            NTypes = GetFASTPar(DataOut,'NTypes');  %get number of LineTypes
+            [DataOut.LineTypes, DataOut.LineTypesHdr] = ParseFASTFmtTable( line, fid, NTypes, true ); %parse the MoorDyn line types table
+            continue;   
+        elseif strcmpi(value,'"Node"') %we've reached the MoorDyn connection properties table (and we think it's a string value so it's in quotes)
+            NConnects = GetFASTPar(DataOut,'NConnects'); %get number of connections (incl. anchors and fairleads)
+            [DataOut.ConProp, DataOut.ConPropHdr] = ParseFASTFmtTable( line, fid, NConnects, true ); %parse the MoorDyn connection properties table
+            continue;   
+        elseif strcmpi(value,'"Line"') %we've reached the MoorDyn line properties table (and we think it's a string value so it's in quotes)
+            NLines = GetFASTPar(DataOut,'NLines'); %get number of line objects  
+            [DataOut.LineProp, DataOut.LinePropHdr] = ParseFASTFmtTable( line, fid, NLines, true ); %parse the MoorDyn line properties table
+            continue;               
+            
         else         
             
             if NextIsMatrix > 0

commit 04f8094138ac1c814419d358e482a0e3b3911695

FAST2Matlab / Matlab2FAST - Bugfix: Use string "OUTPUTS" as indicator that we are reaching the outlist section only when we are dealing with MoorDyn input files. This fixes the following bug introduced by commits 4a847ae and a149ae6: Output section of other input files (like AeroDyn, BeamDyn, ElastoDyn, etc.) were not processed correctly anymore when labeled OUTPUTS. Solution: When the current line contains 'OUTPUTS', it is additionally checked if the data structure has a field 'ConProp' to make shure that we are dealing with a MoorDyn input file.

FAST2MATLAB/FAST2Matlab.m
@@ -105,7 +105,7 @@ while true %loop until discovering Outlist or end of file, than break
         % Check to see if the value is Outlist or OUTPUTS (for MoorDyn)
 
     %if ~isempty(strfind(upper(line),upper('OutList'))) 
-    if ~isempty(strfind( upper(line), upper('OutList') )) || contains(upper(line),upper('OUTPUTS'))
+    if ~isempty(strfind( upper(line), upper('OutList') )) || (contains(upper(line),upper('OUTPUTS')) && isfield(DataOut,'ConProp')) % The second statement is to detect the outlist of MoorDyn input files (Field ConProp will only exist when processing MoorDyn input files.)
         % 6/23/2016: linearization inputs contain "OutList" in the
         % comments, so we need to make sure this is either the first (value) or
         % second (label) word of the line.

MATLAB2FAST/Matlab2FAST.m
@@ -58,7 +58,7 @@ printTableComments = 0;
 NextMatrix = '';
 isInteger = false;
 
-%loop through the template up until OUTLIST, OUTPUTS or end of file
+%loop through the template up until OUTLIST, OUTPUTS (for MoorDyn files) or end of file
 while true
     
     line = fgets(fidIN); %get the next line from the template
@@ -74,7 +74,7 @@ while true
         HaveNewLineChar = true;
     end
     
-    if contains(upper(line),upper('OutList')) || contains(upper(line),upper('OUTPUTS'))
+    if contains(upper(line),upper('OutList')) || (contains(upper(line),upper('OUTPUTS')) && isfield(FastPar,'ConProp')) % The second statement is to detect the outlist of MoorDyn input files (Field ConProp will only exist when processing MoorDyn input files.)
         % 6/23/2016: linearization inputs contain "OutList" in the
         % comments, so we need to make sure this is either the first (value) or
         % second (label) word of the line.