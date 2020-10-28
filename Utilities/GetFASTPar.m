d052f37c1eff5e14b91605a015a359dede4d73bb
Bonnie Jonkman <bonnie.jonkman@envision-energy.com>
2020-10-28 14:56:54 -0600

    update {set|get}FASTpar routines: case insensitive
    also allow user to specify which instance of a label to return if there are multiple ones.

--- Utilities/GetFASTPar.m
+++ Utilities/GetFASTPar.m
@@ -5,16 +5,25 @@
 %
 % Knud A. Kragh
 
-function [OutData,err,Indx]=GetFASTPar(FASTPar,Par)
+function [OutData,err,Indx]=GetFASTPar(FASTPar,Par,ReturnIndx)
 err = false;
 OutData=[];
 Indx=0;
+iCnt = 1;
+if nargin<3
+    ReturnIndx = 1; % by default, return the first time we see this value
+end
+
 if isfield(FASTPar, 'Label')
     for i=1:length(FASTPar.Label)
-        if strcmp(FASTPar.Label{i},Par)==1
+        if strcmpi(FASTPar.Label{i},Par)==1
             OutData=FASTPar.Val{i};
             Indx = i;
-            return
+            if iCnt == ReturnIndx
+                return
+            end
+            
+            iCnt = iCnt + 1;
         end
     end
 end
